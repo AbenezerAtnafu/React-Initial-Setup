4539cd55ec2b0675302ba353f5ca4dbc8842b9b4
Abenezer Atnafu <se.abenezer.atnafu@gmail.com>
2022-03-04 18:06:42 +0300

    feat: tailwind and aliases added

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,18 @@
-const { alias } = require('react-app-rewire-alias');
+const { alias } = require("react-app-rewire-alias");
 
 module.exports = function override(config) {
   alias({
-    '@images': 'src/assets/images',
-    '@styles': 'src/assets/styles',
-    '@components': 'src/components',
-    '@common': 'src/components/common',
-    '@config': 'src/config',
-    '@constants': 'src/constants',
-    '@middleware': 'src/middleware',
-    '@pages': 'src/pages',
-    '@store': 'src/store',
-    '@utils': 'src/utils',
-    '@shared': 'src/shared'
+    "@images": "src/assets/images",
+    "@styles": "src/assets/styles",
+    "@components": "src/components",
+    "@common": "src/components/common",
+    "@config": "src/config",
+    "@constants": "src/constants",
+    "@middleware": "src/middleware",
+    "@pages": "src/pages",
+    "@store": "src/store",
+    "@utils": "src/utils",
+    "@shared": "src/shared",
   })(config);
 
   return config;
